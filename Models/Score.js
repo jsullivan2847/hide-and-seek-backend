d715ec73fea4cb69acb9c5ae8f57e8079ebd2148
James Sullivan <jsullivan2847@gmail.com>
2022-08-19 20:54:31 -0400

    added timestamps

--- Models/Score.js
+++ Models/Score.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const scoreSchema = new mongoose.Schema({
     name: String,
     score: Number,
-})
+}, {timestamps: true})
 
 const Score = mongoose.model('Score', scoreSchema)
 
